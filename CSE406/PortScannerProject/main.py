0acd6452f62cde16de9fbee36eafcc371f0c5bbf
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-08-10 12:10:49 +0600

    added tcp scan

--- CSE406/PortScannerProject/main.py
+++ CSE406/PortScannerProject/main.py
@@ -1,4 +1,8 @@
 from Tkinter import *
+import socket
+import subprocess
+import sys
+from datetime import datetime
 
 
 class PortScanner(Frame):
@@ -31,11 +35,38 @@ class PortScanner(Frame):
                command=self.scan).place(x=180, y=110)
 
     def scan(self):
+        self.listbox.delete(0, 'end')
         v = self.scanType.get()
         if(v == 1):
             print('SYN SCAN')
         if(v == 2):
-            print('TCP Scan')
+            remoteServerIP = socket.gethostbyname('127.0.0.1')
+            t1 = datetime.now()
+            count = 1
+            try:
+                for port in range(1, 1025):
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    result = sock.connect_ex((remoteServerIP, port))
+                    if result == 0:
+                        output = "Port {}: 	 Open\n".format(port)
+                        self.listbox.insert(count, output)
+                        count = count + 1
+                    sock.close()
+
+            except socket.gaierror:
+                output = "Hostname could not be resolved. Exiting"
+                self.listbox.insert(count, output)
+                sys.exit()
+
+            except socket.error:
+                output = "Couldn't connect to server"
+                self.listbox.insert(count, output)
+                sys.exit()
+            t2 = datetime.now()
+            total = t2 - t1
+            output = "Scanning Completed in: {}".format(total)
+
+            self.listbox.insert(count, output)
 
 
 if __name__ == '__main__':
